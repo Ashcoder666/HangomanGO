c19ae3ae074ddc48206e1d7ecc32888801bef5dc
ashir <ashir@metrictreelabs.com>
2023-09-07 06:39:28 +0530

    guessing logic is correct

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	var username string
-	words := []string{"Apple", "Orange", "Blueberry", "Ashir"}
+	words := []string{"apple", "orange", "blueberry", "ashir"}
 	max := len(words)
 	randomNumber := rng.Intn(max-0) - 0
 	fmt.Println(randomNumber)
@@ -20,9 +20,26 @@ func main() {
 	fmt.Println("Get ready to guess this fruit")
 
 	var dashString string = ""
+	var guessLetter string
 	for i := 0; i < len(words[randomNumber]); i++ {
-		dashString = dashString + "_ "
+		dashString = dashString + "_"
 	}
-	fmt.Println(dashString)
+
+	for dashString != words[randomNumber] {
+
+		for i := 0; i < len(words[randomNumber]); i++ {
+			if string(guessLetter) == string(words[randomNumber][i]) {
+				dashString = dashString[:i] + guessLetter + dashString[i+1:]
+			}
+		}
+
+		fmt.Println(dashString)
+		if dashString == words[randomNumber] {
+			fmt.Printf("congrats %v your guess for word %v is correct \n", username, dashString)
+			break
+		}
+		fmt.Println("Enter your letter")
+		fmt.Scan(&guessLetter)
+	}
 
 }
